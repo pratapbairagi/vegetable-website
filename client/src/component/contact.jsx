17d9138c047df7439493c9f83186dc0182841cca
PratapBairagi2 <pratap.bairagi.test@gmail.com>
2024-03-02 16:43:03 +0530

    added cotatc us and changed css

--- client/src/component/contact.jsx
+++ client/src/component/contact.jsx
@@ -12,27 +12,27 @@ const Contact = () => {
                         <img src="./images/contact_msg.png" className="h-full w-full opacity-80" alt="" />
                     </div>
                     <form className="col-span-12 sm:col-span-12 md:col-span-12 lg:col-span-12 grid gird-cols-12 gap-y-2.5 ">
-                        <fieldset className="flex flex-col">
+                        <fieldset className="flex flex-col lg:max-w-60%">
                             <label htmlFor="name" className=" text-sm md:text-base lg:text-xl xl:text-2xl font-semibold font-nunito text-gray-400 px-1">Full Name</label>
                             <input type="text" id="name" className="border-b outline-0 py-1.5 text-xs md:text-sm lg:text-base xl:text-xl md:text-base px-1  text-gray-400 font-nunito rounded" />
                         </fieldset>
 
-                        <fieldset className="flex flex-col">
+                        <fieldset className="flex flex-col lg:max-w-60%">
                             <label htmlFor="email" className=" text-sm md:text-base lg:text-xl xl:text-2xl font-semibold font-nunito text-gray-400 px-1">Email</label>
                             <input type="text" id="email" className="border-b outline-0 py-1.5 text-xs md:text-sm lg:text-base xl:text-xl px-1 py-0.5 text-xs text-gray-400 font-nunito rounded" />
                         </fieldset>
 
-                        <fieldset className="flex flex-col">
+                        <fieldset className="flex flex-col lg:max-w-60%">
                             <label htmlFor="contact" className=" text-sm md:text-base lg:text-xl xl:text-2xl font-semibold font-nunito text-gray-400 px-1">Contact No.</label>
                             <input type="tel" id="contact" className="border-b outline-0 py-1.5 text-xs md:text-sm lg:text-base xl:text-xl px-1 py-0.5 text-xs text-gray-400 font-nunito rounded" />
                         </fieldset>
 
-                        <fieldset className="flex flex-col">
+                        <fieldset className="flex flex-col lg:max-w-60%">
                             <label htmlFor="subject" className="text-sm md:text-base lg:text-xl xl:text-2xl font-semibold font-nunito text-gray-400 px-1">Subject</label>
                             <input type="text" id="contact" className="border-b outline-0 py-1.5 text-xs md:text-sm lg:text-base xl:text-xl px-1 py-0.5 text-xs text-gray-400 font-nunito rounded" />
                         </fieldset>
 
-                        <fieldset className="flex flex-col">
+                        <fieldset className="flex flex-col lg:max-w-60%">
                             <label htmlFor="msg" className=" text-sm md:text-base lg:text-xl xl:text-2xl font-semibold font-nunito text-gray-400 px-1">Message</label>
                             <textarea type="text" rows="6" id="contact" className="border outline-0 px-1 py-2.5 text-xs md:text-sm lg:text-base xl:text-xl text-gray-400 font-nunito rounded" > </textarea>
                         </fieldset>
@@ -49,18 +49,18 @@ const Contact = () => {
                         <div className="order-2 lg:order-1 col-span-12 sm:col-span-12 md:col-span-12 lg:col-span-12 flex flex-col gap-y-4 md:gap-y-6 xl:h-full">
                            
                             <div className="w-full flex flex-col gap-y-0.5">
-                                <span className="text-base md:text-xl lg:text-2xl font-bold font-nunito text-gray-300">Contact No.</span>
-                                <div className="text-xl md:text-2xl lg:text-3xl font-bold font-nunito text-gray-400">+91 8287889123</div>
+                                <span className="text-base md:text-xl font-bold font-nunito text-gray-300">Contact No.</span>
+                                <div className="text-xl md:text-2xl font-bold font-nunito text-gray-400">+91 8287889123</div>
                             </div>
 
                             <div className="w-full flex flex-col gap-y-0.5">
-                                <span className="text-base md:text-xl lg:text-2xl font-bold font-nunito text-gray-300">Email</span>
-                                <div className="text-xl md:text-2xl lg:text-3xl font-bold font-nunito text-gray-400">pratapbairagi@gmail.com</div>
+                                <span className="text-base md:text-xl font-bold font-nunito text-gray-300">Email</span>
+                                <div className="text-xl md:text-2xl font-bold font-nunito text-gray-400">pratapbairagi@gmail.com</div>
                             </div>
 
                             <div className="w-full flex flex-col gap-y-0.5">
-                                <span className="text-base md:text-xl lg:text-2xl font-bold font-nunito text-gray-300">Address</span>
-                                <div className="text-xl md:text-2xl lg:text-3xl font-semibold font-nunito text-gray-400">RZ-170/1A, Tughlakabad Extn. South Delhi, Delhi - 110019.</div>
+                                <span className="text-base md:text-xl font-bold font-nunito text-gray-300">Address</span>
+                                <div className="text-xl md:text-2xl font-semibold font-nunito text-gray-400">RZ-170/1A, Tughlakabad Extn. South Delhi, Delhi - 110019.</div>
                             </div>
 
                         </div>
