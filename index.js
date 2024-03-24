ef627e45fbbf40e7e94ab4bc0018287b1dcc7390
PratapBairagi2 <pratap.bairagi.test@gmail.com>
2024-03-25 00:44:11 +0530

    added server and products fetch from server

--- index.js
+++ index.js
@@ -35,9 +35,9 @@ app.use(fileupload())
 
 app.use("/api", vegetableRouter )
 
-app.use(express.static(path.join(__dirname, "./client/build")))
+app.use(express.static(path.join(__dirname, "./client/dist")))
 app.get("*", (req, res)=>{
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"))
 })
 
 dbConnection();
