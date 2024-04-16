96be0b60d261d8e245a2b63bf5110ec22f6a6ff8
PratapBairagi2 <pratap.bairagi.test@gmail.com>
2024-04-17 02:24:18 +0530

    added tosater functionality and modified action by adding withCredential true 3 forgot to add original server url 9.1

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ const app = express(http);
 app.use(cors(
     {
     credentials : true,
-    // origin : [ "http://localhost:5005", "http://localhost:5173", "https://veg-etable.vercel.app"],
-    origin : ["https://veg-etable.vercel.app"],
+    origin : [ "http://localhost:5005", "http://localhost:5173", "https://veg-etable.vercel.app"],
+    // origin : ["https://veg-etable.vercel.app"],
     // origin : ["http://localhost:5173"],
     methods: "GET, POST, PUT, PATCH, DELETE",
     allowedHeaders: "Content-Type, Authorization"
