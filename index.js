fe4076200a98e993d8188e5e724511416ec3f17a
PratapBairagi2 <pratap.bairagi.test@gmail.com>
2024-04-18 01:43:30 +0530

    added add review popup ui 8

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(cors(
     {
     credentials : true,
     // origin : ["http://localhost:5173", "http://localhost:5005", "https://veg-etable.vercel.app"],
-    origin : ["https://veg-etable.vercel.app"],
+    origin : ["https://veg-etable.vercel.app/"],
     // origin : ["http://localhost:5173"],
     methods: "GET, POST, PUT, PATCH, DELETE",
 
